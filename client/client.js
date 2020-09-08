728af007a9a0e8b480d2d91b782ad01230cabad7
HudsonFinn <11finnh@gmail.com>
2020-09-09 00:51:43 +0100

    feat: Added basic client and form

---
+++ client/client.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+
+    const name = formData.get('name');
+    const content = formData.get('content');
+
+    const chat = {
+        name,
+        content
+    }
+
+    console.log(chat);
+    form.reset();
+});
