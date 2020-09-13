2ab755fc3183a8e143163aa0f564bc8c9027db0d
HudsonFinn <11finnh@gmail.com>
2020-09-13 15:53:56 +0100

    feat: Added database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const monk = require('monk');
 
 const app = express();
+
+const db = monk('localhost/letschat');
+const messages = db.get('messages');
 
 app.use(cors());
 app.use(express.json());
@@ -12,8 +16,22 @@ app.get('/', (request, response) => {
     });
 });
 
+app.get('/message', (request, response) => {
+    messages.find().then( messages => {
+        response.json(messages);
+    });
+});
+
 app.post('/message', (request, response) => {
-    console.log(request.body);
+    const message = {
+        name: request.body.name.toString(),
+        content: request.body.content.toString(),
+        created: new Date()
+    }
+
+    messages.insert(message).then(createdMessage => {
+        response.json(createdMessage);
+    });
 })
 
 app.listen(5000, () => {
